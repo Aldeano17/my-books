b4f5c8596ef7206c945403ab665efbcd9ad496b1
Adrian Marimon <amdev@Adrians-MacBook-Pro.local>
2021-04-06 14:16:58 -0400

    added description to books, and validation of presence: true

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :author)
+    params.require(:book).permit(:title, :author, :description)
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   validates :author, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
